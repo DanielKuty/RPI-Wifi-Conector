3be3977bf533999de4faab2f92bdb9ec5063b669
DanielKuty <daniel.kuty29@gmail.com>
2017-12-12 16:54:12 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import kivy
+kivy.require('1.8.0')
+
+from RPI_configurator.RPI_configuratorapp import Rpi_configuratorApp
+
+if __name__ == "__main__":
+    Rpi_configuratorApp().run()
